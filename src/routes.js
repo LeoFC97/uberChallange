13af4c9d8676c49a23f55402c8a10b27a33847ae
LeoFC97 <leonardo.fc2@outlook.com>
2020-12-12 04:01:43 -0300

    feat: add healthcheck

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,9 @@
 const express = require('express');
 
 const routes = new express.Router();
+const health = require('./health');
 
-routes.get('/test', (req, res) => {
-  console.log('oi');
-  return res.status(200).send({ ok: true });
-});
+routes.get('/test', (req, res) => res.status(200).send({ ok: true }));
+routes.get('/health', health.checkHealth);
 
 module.exports = routes;
