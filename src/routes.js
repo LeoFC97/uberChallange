e48be0bb72ce26856ffaf5fb9df20f790200932c
LeoFC97 <leonardo.fc2@outlook.com>
2020-12-12 18:34:11 -0300

    fix: removed unused endpoint

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,6 @@ const routes = new express.Router();
 const health = require('./health');
 const mail = require('./controllers/mail');
 
-routes.get('/test', (req, res) => res.status(200).send({ ok: true }));
 routes.get('/health', health.checkHealth);
 routes.post('/sendMail', mail.sendMail);
 
