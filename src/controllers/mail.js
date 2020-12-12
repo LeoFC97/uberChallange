d9a99d0612c328928529e1c3d431a79b1d64d83d
LeoFC97 <leonardo.fc2@outlook.com>
2020-12-12 18:35:11 -0300

    feat: check if sended parms

--- src/controllers/mail.js
+++ src/controllers/mail.js
@@ -1,11 +1,17 @@
 const sparkPost = require('../services/sparkPost');
+const errorHandler = require('./errorHandler');
 
 module.exports = {
   async sendMail(req, res) {
-    const emailSended = await sparkPost.sendMailToSparkApi(req.body);
-    console.log(emailSended.errors[0]);
+    const {
+      from, subject, emailBody, recipients,
+    } = req.body;
+    if (!from || !subject || !emailBody || !recipients) { // valid better
+      const { code, msg } = errorHandler.missingParamsError();
+      return res.status(code).send({ error: msg });
+    }
+    const emailSended = await sparkPost.sendMailToSparkApi(from, subject, emailBody, recipients);
     if (emailSended.errors[0]) {
-      console.log(emailSended.errors[0]);
       return res.status(emailSended.statusCode).send(emailSended.errors[0]);
     }
     return res.status(200).send({ emailSended });
