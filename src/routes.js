8f131a2690ccefdf4152672a03e1205dd954ea69
LeoFC97 <leonardo.fc2@outlook.com>
2020-12-12 04:50:37 -0300

    feat: add mail endpoint

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const routes = new express.Router();
 const health = require('./health');
+const mail = require('./controllers/mail');
 
 routes.get('/test', (req, res) => res.status(200).send({ ok: true }));
 routes.get('/health', health.checkHealth);
+routes.post('/sendMail', mail.sendMail);
 
 module.exports = routes;
