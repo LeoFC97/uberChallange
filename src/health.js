2617249cc3c69f3b4356223267dc3077a1c0be83
LeoFC97 <leonardo.fc2@outlook.com>
2020-12-12 03:57:57 -0300

    fix: remove logs

--- src/health.js
+++ src/health.js
@@ -1,9 +1,8 @@
-module.exports={
-  checkHealth (req,res){
-    console.log(`'`)
+module.exports = {
+  checkHealth(req, res) {
     const response = {
       date_time: new Date().toISOString(),
     };
-    return res.status(200).send(response)
-  }
-}
+    return res.status(200).send(response);
+  },
+};
