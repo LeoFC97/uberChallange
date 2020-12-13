a75b030bdc8ad3cbfbb6ca9d7c3494eed70a6378
LeoFC97 <leonardo.fc2@outlook.com>
2020-12-13 16:34:09 -0300

    fix: rename file

--- src/controllers/errorHandler.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  missingParamsError() {
-    const errorToReturn = {
-      code: 400,
-      msg: 'Missing Params, check the api manual for more info',
-    };
-    return errorToReturn;
-  },
-};
